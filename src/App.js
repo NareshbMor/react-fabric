471a0971a6c5452511f0879c9e505e8d7febb93d
unknown <naresh.mor.web@gmail.com>
2021-05-22 18:07:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Are you there?
         </p>
         <h1>Hello, NaresH MoR :)</h1>
       </header>
